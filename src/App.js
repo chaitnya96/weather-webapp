72ffd7b93e45070f4d01bfae586bcd1a349afb5a
Chaitnya parikh <chaitnyaparikh@gmail.com>
2019-10-10 03:33:35 +0530

    weather api data view

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import React from "react";
-import { Container } from "reactstrap";
-import Calendar from "react-calendar";
+import { Calendar, Row, Col, Spin, Layout } from "antd";
 import axios from "axios";
 import WeatherInformation from "./WeatherInformation";
+import moment from "moment";
+import "./styles.css";
 
 class App extends React.Component {
   state = {
-    date: new Date()
+    date: moment().format("YYYY-MM-DD")
   };
-
-  onChange = date => this.setState({ date });
-  render() {
+  componentDidMount() {
+    this.getDateDetails(this.state.date);
+  }
+  getDateDetails = date => {
     axios
       .post(`http://interview.com360degree.com/api/getWeather`, {
-        date: this.state
+        // date: new Date(2019, 9, 11, 22, 30, 30, 0)
+        date
       })
-      .then(res => console.log(res))
+      .then(res => {
+        this.setState({
+          weatherData: res.data.data[0]
+        });
+      })
       .catch(err => console.log(err));
+  };
+  onPanelChange = date =>
+    this.setState(
+      { date: moment(date).format("YYYY/MM/DD") },
+      this.getDateDetails(this.state.date)
+    );
+  render() {
+    const { weatherData, date } = this.state;
     return (
-      <Container>
-        <Calendar onChange={this.onChange} value={this.state.date} />
-        <WeatherInformation />
-      </Container>
+      <div className="todoContainer">
+        <Row>
+          <Col
+            sm="12"
+            style={{ width: 500, border: "1px solid #d9d9d9", borderRadius: 4 }}
+          >
+            <Calendar fullscreen={false} onSelect={this.onPanelChange} />
+          </Col>
+          <Col>
+            {weatherData ? (
+              <WeatherInformation weatherData={weatherData} date={date} />
+            ) : (
+              <Spin size="large" />
+            )}
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
--- src/WeatherInformation.jsx
+++ src/WeatherInformation.jsx
@@ -1,11 +1,77 @@
 import React, { Component } from "react";
+import { Card, Label, Row, Col } from "antd";
+import moment from "moment";
 
 class WeatherInformation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCurrentDate: true,
+      selectedForecast: {}
+    };
+  }
+  componentDidMount() {
+    const { weatherData, date } = this.props;
+    const { forecast } = weatherData;
+    console.log(date, forecast.filter(data => data.date === date));
+  }
+  componentWillReceiveProps(nextProps) {
+    const { forecast, current } = this.props.weatherData;
+    let date = moment(nextProps.date).format("YYYY-MM-DD");
+    if (date != moment().format("YYYY-MM-DD")) {
+      let selectedForecast = forecast.filter(data => data.date === date)[0];
+      console.log(selectedForecast);
+      if (selectedForecast) {
+        this.setState({
+          isCurrentDate: false,
+          selectedForecast
+        });
+      } else {
+        this.setState({
+          isCurrentDate: false,
+          selectedForecast: {
+            ...current,
+            date,
+            day: moment(date).format("dddd")
+          }
+        });
+      }
+    } else {
+      this.setState({
+        isCurrentDate: true
+      });
+    }
+  }
+  getWeatherData = data => {
+    // add the key name into dataToDisplay array to display that data
+    // using this we removes unwanted data.
+    let dataToDisplay = ["day", "skytext", "skytextday", "date"];
+    let weatherData = [];
+    for (let key in data) {
+      console.log(data);
+      if (dataToDisplay.includes(key)) {
+        let value = data[key];
+        weatherData.push(
+          <div>
+            <label htmlFor={key}>{key}</label>:- <span id={key}>{value}</span>
+          </div>
+        );
+      }
+    }
+    return weatherData;
+  };
   render() {
+    const { location, current } = this.props.weatherData;
+    const { selectedForecast, isCurrentDate } = this.state;
+    let data = isCurrentDate ? current : selectedForecast;
     return (
-      <div>
-        <h1>Weather Information</h1>
-      </div>
+      <Row>
+        <Col sm={8}>
+          <Card title={location.name} bordered={false}>
+            {this.getWeatherData(data)}
+          </Card>
+        </Col>
+      </Row>
     );
   }
 }
